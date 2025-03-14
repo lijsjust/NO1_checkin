787140cb1fbc9924ddf80e47635907af6897942a
lijsjust <103588342+lijsjust@users.noreply.github.com>
2025-03-14 17:34:22 +0800

    Update Cloud189.js

--- Cloud189.js
+++ Cloud189.js
@@ -288,7 +288,7 @@ const main = async () => {
     logger.log("\n\n");
     const events = recording.replay();
     const content = events.map((e) => `${e.data.join("")}`).join("  \n");
-    push("天翼云盘自动签到任务", content);
+    push("NO1天翼云盘自动签到任务", content);
     recording.erase();
   }
 })();
